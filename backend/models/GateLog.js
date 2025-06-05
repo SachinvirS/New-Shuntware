68cfa93fa23f882f64cef2157add2dca15e07ae2
SachinvirS <sachin@gbsenterprises.ca>
2025-06-04 21:16:55 -0700

    Update GateLog.js

--- backend/models/GateLog.js
+++ backend/models/GateLog.js
@@ -3,27 +3,17 @@ import mongoose from "mongoose";
 
 const gateLogSchema = new mongoose.Schema({
   driverName: String,
-  ppe: Boolean,
-  notes: String,
-  vehicleCarrier: String,
-  vehiclePlate: String,
-  vehicleNumber: String,
   trailerNumber: String,
-  trailerCarrier: String,
-  trailerCategory: String,
-  trailerSize: String,
-  trailerProperty: String,
+  unitNumber: String,
+  carrier: String,
+  licensePlate: String,
   seal: String,
-  trailerComments: String,
-  loadAssignment: String,
-  billOfLading: String,
-  trailerLoadProperty: String,
-  trailerLoadComment: String,
-  destination: String,
-  type: String, // "Entry" or "Exit"
+  location: String,
+  ppe: String,
+  notes: String,
+  type: String, // Entry or Exit
   currentZone: String,
   timestamp: { type: Date, default: Date.now }
 });
 
-const GateLog = mongoose.model("GateLog", gateLogSchema);
-export default GateLog;
+export default mongoose.model("GateLog", gateLogSchema);
